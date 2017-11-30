3cadd10eae48ee5f92af57b0527144d7ae68bd5a
Jon Tvermose Nielsen <jtn@it-minds.dk>
2017-11-30 10:19:36 +0100

    Stuffz, all of it (Y)

--- Assets/GoogleARCore/HelloARExample/Scripts/HelloARController.cs
+++ Assets/GoogleARCore/HelloARExample/Scripts/HelloARController.cs
@@ -47,73 +47,47 @@ namespace GoogleARCore.HelloAR
         public GameObject m_trackedPlanePrefab;
 
         /// <summary>
-        /// A model to place when a raycast from a user touch hits a plane.
-        /// </summary>
-        //public GameObject m_andyAndroidPrefab;
-
-        /// <summary>
         /// A gameobject parenting UI for displaying the "searching for planes" snackbar.
         /// </summary>
         public GameObject m_searchingForPlaneUI;
 
-        public List<GameObject> planes = new List<GameObject>();
+        public GameObject ButtonBar;
+        public GameObject tilePrefab;
+        public ParticleSystem embersEffect;
+        public ParticleSystem fireTouch;
 
-        private Spawner spawnerScript;
-
+        public GameObject[,] tilesArray;
         public int[,] boardItemsArray;
+
+
+        #region Private stuff
+
+        private List<TrackedPlane> m_newPlanes = new List<TrackedPlane>();
+        private List<TrackedPlane> m_allPlanes = new List<TrackedPlane>();
+        private SessionComponent _arCoreDevice;
+        private List<GameObject> planes = new List<GameObject>();
+        
+        public GameObject parentCubePrefab;
+        private Spawner spawnerScript;
+        private List<GameObject> tiles = new List<GameObject>();
+        
         private int levelSizeX = 18;
         private int levelSizeZ = 14;
-
         private int emptyPlacement = 0;
         private int playerPlacement = -1;
         private int pickupPlacement = 1;
         private int wallPlacement = 2;
         private int goalPlacement = 3;
         private int obstaclePlacement = 4;
-
         private int elementNumber = 0;
 
-        public GameObject cubePrefab;
+        private Renderer _rend;
 
-        public GameObject tilePrefab;
-        public List<GameObject> tiles = new List<GameObject>();
-        public GameObject[,] tilesArray;
-
-        public GameObject parentCubePrefab;
-
-        public ParticleSystem embersEffect;
-
-        public ParticleSystem fireTouch;
-
-        public List<GameObject> cubes = new List<GameObject>();
-
-        private List<TrackedPlane> m_newPlanes = new List<TrackedPlane>();
-
-        private List<TrackedPlane> m_allPlanes = new List<TrackedPlane>();
-
-        private Color[] m_planeColors = new Color[] {
-            new Color(1.0f, 1.0f, 1.0f),
-            new Color(0.956f, 0.262f, 0.211f),
-            new Color(0.913f, 0.117f, 0.388f),
-            new Color(0.611f, 0.152f, 0.654f),
-            new Color(0.403f, 0.227f, 0.717f),
-            new Color(0.247f, 0.317f, 0.709f),
-            new Color(0.129f, 0.588f, 0.952f),
-            new Color(0.011f, 0.662f, 0.956f),
-            new Color(0f, 0.737f, 0.831f),
-            new Color(0f, 0.588f, 0.533f),
-            new Color(0.298f, 0.686f, 0.313f),
-            new Color(0.545f, 0.764f, 0.290f),
-            new Color(0.803f, 0.862f, 0.223f),
-            new Color(1.0f, 0.921f, 0.231f),
-            new Color(1.0f, 0.756f, 0.027f)
-        };
-
-        private SessionComponent _arCoreDevice;
-        public Renderer _rend;
+#endregion
 
         public void Start()
         {
+            ButtonBar.SetActive(false);
             spawnerScript = gameObject.GetComponent<Spawner>();
             makePickupArray();
             _arCoreDevice = GameObject.FindGameObjectWithTag("ARCoreDevice").GetComponent<SessionComponent>();
@@ -135,46 +109,48 @@ namespace GoogleARCore.HelloAR
                 Application.Quit();
             }
 
-            // The tracking state must be FrameTrackingState.Tracking in order to access the Frame.
-            if (Frame.TrackingState != FrameTrackingState.Tracking)
+            if (tiles.Count == 0)
             {
-                const int LOST_TRACKING_SLEEP_TIMEOUT = 15;
-                Screen.sleepTimeout = LOST_TRACKING_SLEEP_TIMEOUT;
-                return;
-            }
-            Screen.sleepTimeout = SleepTimeout.NeverSleep;
-            Frame.GetNewPlanes(ref m_newPlanes);
-           
-            // Iterate over planes found in this frame and instantiate corresponding GameObjects to visualize them.
-            for (int i = 0; i < m_newPlanes.Count; i++)
-            {
-                // Instantiate a plane visualization prefab and set it to track the new plane. The transform is set to
-                // the origin with an identity rotation since the mesh for our prefab is updated in Unity World
-                // coordinates.
-                GameObject planeObject = Instantiate(m_trackedPlanePrefab, Vector3.zero, Quaternion.identity, transform);
-                planeObject.GetComponent<TrackedPlaneVisualizer>().SetTrackedPlane(m_newPlanes[i]);
-
-                // Apply a random color and grid rotation.
-                //planeObject.GetComponent<Renderer>().material.SetColor("_GridColor", m_planeColors[Random.Range(0,
-                //    m_planeColors.Length - 1)]);
-                planeObject.GetComponent<Renderer>().material.SetColor("_GridColor", Color.green);
-                planeObject.GetComponent<Renderer>().material.SetFloat("_UvRotation", Random.Range(0.0f, 360.0f));
-                planes.Add(planeObject);
-            }
-
-            // Disable the snackbar UI when no planes are valid.
-            bool showSearchingUI = true;
-            Frame.GetAllPlanes(ref m_allPlanes);
-            for (int i = 0; i < m_allPlanes.Count; i++)
-            {
-                if (m_allPlanes[i].IsValid)
+                // The tracking state must be FrameTrackingState.Tracking in order to access the Frame.
+                if (Frame.TrackingState != FrameTrackingState.Tracking)
                 {
-                    showSearchingUI = false;
-                    break;
+                    const int LOST_TRACKING_SLEEP_TIMEOUT = 15;
+                    Screen.sleepTimeout = LOST_TRACKING_SLEEP_TIMEOUT;
+                    return;
                 }
-            }
+                Screen.sleepTimeout = SleepTimeout.NeverSleep;
+                Frame.GetNewPlanes(ref m_newPlanes);
 
-            m_searchingForPlaneUI.SetActive(showSearchingUI);
+                // Iterate over planes found in this frame and instantiate corresponding GameObjects to visualize them.
+                for (int i = 0; i < m_newPlanes.Count; i++)
+                {
+                    // Instantiate a plane visualization prefab and set it to track the new plane. The transform is set to
+                    // the origin with an identity rotation since the mesh for our prefab is updated in Unity World
+                    // coordinates.
+                    GameObject planeObject = Instantiate(m_trackedPlanePrefab, Vector3.zero, Quaternion.identity, transform);
+                    planeObject.GetComponent<TrackedPlaneVisualizer>().SetTrackedPlane(m_newPlanes[i]);
+
+                    // Apply a random color and grid rotation.
+                    //planeObject.GetComponent<Renderer>().material.SetColor("_GridColor", m_planeColors[Random.Range(0,
+                    //    m_planeColors.Length - 1)]);
+                    planeObject.GetComponent<Renderer>().material.SetColor("_GridColor", Color.green);
+                    planeObject.GetComponent<Renderer>().material.SetFloat("_UvRotation", Random.Range(0.0f, 360.0f));
+                    planes.Add(planeObject);
+                }
+
+                // Disable the snackbar UI when no planes are valid.
+                bool showSearchingUI = true;
+                Frame.GetAllPlanes(ref m_allPlanes);
+                for (int i = 0; i < m_allPlanes.Count; i++)
+                {
+                    if (m_allPlanes[i].IsValid)
+                    {
+                        showSearchingUI = false;
+                        break;
+                    }
+                }
+                m_searchingForPlaneUI.SetActive(showSearchingUI);
+            }
 
             Touch touch;
             if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began || IsUiTouch(touch))
@@ -193,21 +169,22 @@ namespace GoogleARCore.HelloAR
                 }
                 _arCoreDevice.m_arSessionConfig.m_enablePlaneFinding = false; // Disable plane tracking in AR CORE
                 PlaceElement(touch);
-
-                
-            } else
+                PlaceFireTouch(touch);
+                ButtonBar.SetActive(true);
+            }
+            else
             {
-                foreach (GameObject plane in planes)
-                {
-                    plane.SetActive(true);
-                }
-                foreach (var tile in tiles)
-                {
-                    Destroy(tile);
-                }
-                tiles = new List<GameObject>();
-                spawnerScript.DestroyAll();
-                _arCoreDevice.m_arSessionConfig.m_enablePlaneFinding = true; // Enable plane tracking in AR CORE
+                //foreach (GameObject plane in planes)
+                //{
+                //    plane.SetActive(true);
+                //}
+                //foreach (var tile in tiles)
+                //{
+                //    Destroy(tile);
+                //}
+                //tiles = new List<GameObject>();
+                //spawnerScript.DestroyAll();
+                //_arCoreDevice.m_arSessionConfig.m_enablePlaneFinding = true; // Enable plane tracking in AR CORE
             }
         }
 
